d3fd05291d233ca1b17ac9004bf93068f6b53f65
wzv5 <wzv5@msn.com>
2017-03-24 14:48:48 +0800

    添加项目文件。

---
+++ IsaacSaveBackup/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace IsaacSaveBackup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开存档目录ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开备份目录ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.清空所有备份ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Steam用户名：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(99, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(162, 20);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.Filter = "*gamestate1.dat";
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            this.fileSystemWatcher1.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Created);
+            this.fileSystemWatcher1.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Deleted);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(249, 208);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "暂停监控";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "恢复存档";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(272, 25);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开存档目录ToolStripMenuItem,
+            this.打开备份目录ToolStripMenuItem,
+            this.清空所有备份ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 打开存档目录ToolStripMenuItem
+            // 
+            this.打开存档目录ToolStripMenuItem.Name = "打开存档目录ToolStripMenuItem";
+            this.打开存档目录ToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.打开存档目录ToolStripMenuItem.Text = "打开游戏存档目录";
+            this.打开存档目录ToolStripMenuItem.Click += new System.EventHandler(this.打开存档目录ToolStripMenuItem_Click);
+            // 
+            // 打开备份目录ToolStripMenuItem
+            // 
+            this.打开备份目录ToolStripMenuItem.Name = "打开备份目录ToolStripMenuItem";
+            this.打开备份目录ToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.打开备份目录ToolStripMenuItem.Text = "打开备份目录";
+            this.打开备份目录ToolStripMenuItem.Click += new System.EventHandler(this.打开备份目录ToolStripMenuItem_Click);
+            // 
+            // 清空所有备份ToolStripMenuItem
+            // 
+            this.清空所有备份ToolStripMenuItem.Name = "清空所有备份ToolStripMenuItem";
+            this.清空所有备份ToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.清空所有备份ToolStripMenuItem.Text = "清空所有备份";
+            this.清空所有备份ToolStripMenuItem.Click += new System.EventHandler(this.清空所有备份ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 310);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "以撒存档自动备份";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开存档目录ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开备份目录ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 清空所有备份ToolStripMenuItem;
+    }
+}
+
---
+++ IsaacSaveBackup/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace IsaacSaveBackup
+{
+    public partial class Form1 : Form
+    {
+        private string _steampath;
+        private string _lastsave;
+
+        private class ComboBoxItem
+        {
+            public string Name { get; set; }
+            public string ID { get; set; }
+            
+            public ComboBoxItem(string name = "", string id = "")
+            {
+                Name = name;
+                ID = id;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0} [{1}]", Name, ID);
+            }
+        }
+
+        private class ListBoxItem
+        {
+            public string Text { get; set; }
+            public string Tag { get; set; }
+
+            public ListBoxItem(string text = "", string tag = "")
+            {
+                Text = text;
+                Tag = tag;
+            }
+
+            public override string ToString()
+            {
+                return Text;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Valve\\Steam");
+            if (key == null)
+            {
+                MessageBox.Show("没有安装 Steam", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+            _steampath = key.GetValue("SteamPath") as string;
+            foreach (var i in key.OpenSubKey("Users").GetSubKeyNames())
+            {
+                var p = Path.Combine(_steampath, "userdata", i, "config", "localconfig.vdf");
+                if (File.Exists(p))
+                {
+                    var m = Regex.Match(File.ReadAllText(p), "\"PersonaName\"\t\t\"(.+)\"");
+                    var name = m.Groups[1].Value;
+                    if (Directory.Exists(Path.Combine(_steampath, "userdata", i, "250900")))
+                    {
+                        var index= comboBox1.Items.Add(new ComboBoxItem(name, i));
+                    }
+                }
+
+            }
+            if (comboBox1.Items.Count == 0)
+            {
+                MessageBox.Show("没有找到以撒存档目录，请先安装游戏，并至少运行一次。", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var item = comboBox1.SelectedItem as ComboBoxItem;
+            fileSystemWatcher1.Path = Path.Combine(_steampath, "userdata", item.ID, "250900", "remote");
+            fileSystemWatcher1.EnableRaisingEvents = true;
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, FileSystemEventArgs e)
+        {
+            onFileChanged();
+        }
+
+        private void fileSystemWatcher1_Created(object sender, FileSystemEventArgs e)
+        {
+            onFileChanged();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool FlashWindow(IntPtr hWnd, bool bInvert);
+
+        private void onFileChanged()
+        {
+            Thread.Sleep(1000); // 延迟1秒执行，避免文件占用
+            var now = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            var dist = Path.Combine(Application.StartupPath, "save", now);
+            if (Directory.Exists(dist))
+            {
+                return;
+            }
+            Directory.CreateDirectory(Path.Combine(dist, "remote"));
+            var source = fileSystemWatcher1.Path;
+            foreach (var i in Directory.GetFiles(source))
+            {
+                File.Copy(i, Path.Combine(dist, "remote", Path.GetFileName(i)));
+            }
+            File.Copy(Path.Combine(Directory.GetParent(source).FullName, "remotecache.vdf"), Path.Combine(dist, "remotecache.vdf"));
+            _lastsave = dist;
+            listBox1.Items.Add(new ListBoxItem(string.Format("[{0}] 存档备份已创建", now), dist));
+            FlashWindow(this.Handle, true);
+        }
+
+        private void fileSystemWatcher1_Deleted(object sender, FileSystemEventArgs e)
+        {
+            if (string.IsNullOrEmpty(_lastsave))
+                return;
+
+            var ret = MessageBox.Show("角色已死亡，是否还原到最近一次的备份？（这将自动关闭游戏）", "询问", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (ret == DialogResult.Yes)
+            {
+                restoresave(_lastsave);
+            }
+        }
+
+        private void restoresave(string dist)
+        {
+            fileSystemWatcher1.EnableRaisingEvents = false;
+            foreach (var p in Process.GetProcessesByName("isaac-ng"))
+            {
+                p.Kill();
+                p.WaitForExit();
+            }
+            foreach (var i in Directory.GetFiles(Path.Combine(dist, "remote")))
+            {
+                File.Copy(i, Path.Combine(fileSystemWatcher1.Path, Path.GetFileName(i)), true);
+            }
+            File.Copy(Path.Combine(dist, "remotecache.vdf"), Path.Combine(Directory.GetParent(fileSystemWatcher1.Path).FullName, "remotecache.vdf"), true);
+            fileSystemWatcher1.EnableRaisingEvents = true;
+            var ret = MessageBox.Show("已还原存档，是否启动游戏？", "询问", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (ret == DialogResult.Yes)
+            {
+                Process.Start("steam://rungameid/250900");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "暂停监控")
+            {
+                fileSystemWatcher1.EnableRaisingEvents = false;
+                button1.Text = "开始监控";
+            }
+            else
+            {
+                fileSystemWatcher1.EnableRaisingEvents = true;
+                button1.Text = "暂停监控";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var item = listBox1.SelectedItem as ListBoxItem;
+            if (item == null)
+            {
+                MessageBox.Show("请选中想要还原的存档", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            var ret = MessageBox.Show("确定要恢复到选中的备份吗？（这将自动关闭游戏）", "询问", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (ret == DialogResult.Yes)
+            {
+                restoresave(item.Tag);
+            }
+        }
+
+        private void 打开存档目录ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start(Directory.GetParent(fileSystemWatcher1.Path).FullName);
+        }
+
+        private void 打开备份目录ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(Path.Combine(Application.StartupPath, "save")))
+            {
+                MessageBox.Show("当前没有任何备份", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Process.Start(Path.Combine(Application.StartupPath, "save"));
+        }
+
+        private void 清空所有备份ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Directory.Delete(Path.Combine(Application.StartupPath, "save"), true);
+            }
+            catch (Exception)
+            {
+            }
+            listBox1.Items.Clear();
+            _lastsave = "";
+        }
+    }
+}
